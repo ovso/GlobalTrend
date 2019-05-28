1d5c6f10f8dfb1a8dd4232a8092024e111a5ae8e
jaeho <ovsoce@gmail.com>
2019-05-29 07:08:19 +0900

    WIP RealTimeTrend

--- app/src/main/java/io/github/ovso/globaltrend/view/ui/main/realtimetrend/RealTimeTrendFragment.kt
+++ app/src/main/java/io/github/ovso/globaltrend/view/ui/main/realtimetrend/RealTimeTrendFragment.kt
@@ -1,15 +1,20 @@
 package io.github.ovso.globaltrend.view.ui.main.realtimetrend
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import io.github.ovso.globaltrend.R
+import io.github.ovso.globaltrend.databinding.FragmentRealTimeTrendBinding
+import io.github.ovso.globaltrend.view.adapter.MainAdapter
+import kotlinx.android.synthetic.main.fragment_real_time_trend.recyclerview_real_time
 
 class RealTimeTrendFragment : Fragment() {
+  private val adapter = MainAdapter()
 
   companion object {
     fun newInstance() = RealTimeTrendFragment()
@@ -18,16 +23,46 @@ class RealTimeTrendFragment : Fragment() {
   private lateinit var viewModel: RealTimeTrendViewModel
 
   override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
+    inflater: LayoutInflater,
+    container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return inflater.inflate(R.layout.fragment_real_time_trend, container, false)
+    viewModel = provideViewModel()
+    val binding = inflateDataBinding(inflater, container)
+    binding.viewModel = viewModel
+    return binding.root
   }
+
+  private fun inflateDataBinding(
+    inflater: LayoutInflater,
+    container: ViewGroup?
+  ): FragmentRealTimeTrendBinding {
+    return DataBindingUtil.inflate(
+      inflater,
+      R.layout.fragment_real_time_trend,
+      container,
+      false
+    )
+  }
+
+  private fun provideViewModel() =
+    ViewModelProviders.of(this).get(RealTimeTrendViewModel::class.java)
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
-    viewModel = ViewModelProviders.of(this).get(RealTimeTrendViewModel::class.java)
-    // TODO: Use the ViewModel
+    setupRev()
+    obRevListData()
+    //viewModel.fetchList()
   }
 
+  private fun obRevListData() {
+    viewModel.elementsLiveData.observe(this, Observer {
+      adapter.elements = it
+      adapter.notifyDataSetChanged()
+    })
+  }
+
+  private fun setupRev() {
+    recyclerview_real_time.adapter = adapter
+  }
 }
