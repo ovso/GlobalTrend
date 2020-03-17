40ed177c340934da4c528ee965f3883b193f0ceb
jaeho <ovsoce@gmail.com>
2020-03-18 04:35:47 +0900

    Apply adaptive banner on CountryActivity

--- app/src/main/java/io/github/ovso/globaltrend/view/ui/country/CountryActivity.kt
+++ app/src/main/java/io/github/ovso/globaltrend/view/ui/country/CountryActivity.kt
@@ -3,7 +3,9 @@ package io.github.ovso.globaltrend.view.ui.country
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.github.ovso.globaltrend.R
+import io.github.ovso.globaltrend.extension.loadAdaptiveBanner
 import kotlinx.android.synthetic.main.activity_web.*
+import kotlinx.android.synthetic.main.layout_banner_container.*
 
 class CountryActivity : AppCompatActivity() {
 
@@ -19,5 +21,6 @@ class CountryActivity : AppCompatActivity() {
 
     setSupportActionBar(toolbar)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    loadAdaptiveBanner(ff_all_banner_container, getString(R.string.ads_banner_unit_id))
   }
 }
