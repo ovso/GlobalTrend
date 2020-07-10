127e6b4d42902b6bb709071c0b01f74c76999e00
jaeho <ovsoce@gmail.com>
2020-07-10 23:29:37 +0900

    Added hilt

--- app/src/main/java/io/github/ovso/globaltrend/view/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/ovso/globaltrend/view/ui/main/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.navigation.NavigationView
 import com.pixplicity.easyprefs.library.Prefs
+import dagger.hilt.android.AndroidEntryPoint
 import de.psdev.licensesdialog.LicensesDialog
 import io.github.ovso.globaltrend.R
 import io.github.ovso.globaltrend.R.id
