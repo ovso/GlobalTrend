f4789a46dd0f567e32fe17e2720f58eb75c940a1
jaeho <ovsoce@gmail.com>
2020-07-11 16:26:10 +0900

    Hilt test

--- app/src/main/java/io/github/ovso/globaltrend/di/MainActivityModule.kt
+++ app/src/main/java/io/github/ovso/globaltrend/di/MainActivityModule.kt
@@ -3,15 +3,13 @@ package io.github.ovso.globaltrend.di
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityRetainedComponent
-import dagger.hilt.android.scopes.ActivityRetainedScoped
+import dagger.hilt.android.components.ApplicationComponent
 
 @Module
-@InstallIn(ActivityRetainedComponent::class)
+@InstallIn(ApplicationComponent::class)
 object MainActivityModule {
 
   @Provides
-  @ActivityRetainedScoped
   fun provideMainRepository(): String {
     return "HelloWorld"
   }
